049092404f2ce95457cae5bf39e4d202d2d4e263
GrosFatPower <jlouis_griffon@yahoo.fr>
2025-08-29 11:07:53 +0200

    .

--- Source/src/RasterData.h
+++ Source/src/RasterData.h
@@ -82,10 +82,45 @@ namespace RasterData
       __m256 attrib2 = _mm256_set_ps(iAttrib2._WorldPos.x, iAttrib2._WorldPos.y, iAttrib2._WorldPos.z, iAttrib2._UV.x, iAttrib2._UV.y, iAttrib2._Normal.x, iAttrib2._Normal.y, iAttrib2._Normal.z);
       __m256 attrib3 = _mm256_set_ps(iAttrib3._WorldPos.x, iAttrib3._WorldPos.y, iAttrib3._WorldPos.z, iAttrib3._UV.x, iAttrib3._UV.y, iAttrib3._Normal.x, iAttrib3._Normal.y, iAttrib3._Normal.z);
 
+      InterpolateAVX2(attrib1, attrib2, attrib3, iWeights, oResult);
+    }
+
+    static Varying InterpolateAVX2(const RasterData::Varying & iAttrib1, const RasterData::Varying & iAttrib2, const RasterData::Varying & iAttrib3, const float iWeights[3])
+    {
+      Varying result;
+      Varying::InterpolateAVX2(iAttrib1, iAttrib2, iAttrib3, iWeights, result);
+      return result;
+    }
+#endif
+
+#ifdef SIMD_ARM_NEON
+    static void InterpolateARM(const float32x4_t & iAttrib1, const float32x4_t & iAttrib2, const float32x4_t & iAttrib3, const float iWeights[3], Varying & oResult)
+    {
+      float32x4_t Weights[3] = { _mm256_set1_ps(iWeights[0]), _mm256_set1_ps(iWeights[1]), _mm256_set1_ps(iWeights[2]) };
+
+      float32x4_t interpolResult;
+      SIMDUtils::InterpolateAVX2(iAttrib1, iAttrib2, iAttrib3, Weights, interpolResult);
+
+      oResult._WorldPos.x = interpolResult.m256_f32[7];
+      oResult._WorldPos.y = interpolResult.m256_f32[6];
+      oResult._WorldPos.z = interpolResult.m256_f32[5];
+      oResult._UV.x       = interpolResult.m256_f32[4];
+      oResult._UV.y       = interpolResult.m256_f32[3];
+      oResult._Normal.x   = interpolResult.m256_f32[2];
+      oResult._Normal.y   = interpolResult.m256_f32[1];
+      oResult._Normal.z   = interpolResult.m256_f32[0];
+    }
+
+    static void InterpolateARM(const RasterData::Varying & iAttrib1, const RasterData::Varying & iAttrib2, const RasterData::Varying & iAttrib3, const float iWeights[3], Varying & oResult)
+    {
+      float32x4_t attrib1 = _mm256_set_ps(iAttrib1._WorldPos.x, iAttrib1._WorldPos.y, iAttrib1._WorldPos.z, iAttrib1._UV.x, iAttrib1._UV.y, iAttrib1._Normal.x, iAttrib1._Normal.y, iAttrib1._Normal.z);
+      float32x4_t attrib2 = _mm256_set_ps(iAttrib2._WorldPos.x, iAttrib2._WorldPos.y, iAttrib2._WorldPos.z, iAttrib2._UV.x, iAttrib2._UV.y, iAttrib2._Normal.x, iAttrib2._Normal.y, iAttrib2._Normal.z);
+      float32x4_t attrib3 = _mm256_set_ps(iAttrib3._WorldPos.x, iAttrib3._WorldPos.y, iAttrib3._WorldPos.z, iAttrib3._UV.x, iAttrib3._UV.y, iAttrib3._Normal.x, iAttrib3._Normal.y, iAttrib3._Normal.z);
+
       Varying::InterpolateAVX2(attrib1, attrib2, attrib3, iWeights, oResult);
     }
 
-    static RasterData::Varying InterpolateAVX2(const RasterData::Varying & iAttrib1, const RasterData::Varying & iAttrib2, const RasterData::Varying & iAttrib3, const float iWeights[3])
+    static RasterData::Varying InterpolateARM(const RasterData::Varying & iAttrib1, const RasterData::Varying & iAttrib2, const RasterData::Varying & iAttrib3, const float iWeights[3])
     {
       Varying result;
       Varying::InterpolateAVX2(iAttrib1, iAttrib2, iAttrib3, iWeights, result);
