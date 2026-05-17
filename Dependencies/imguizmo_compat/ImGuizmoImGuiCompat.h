9a3a62d7d6c669f03c9812fdf7800639b6628904
GrosFatPower <jlouis_griffon@yahoo.fr>
2026-05-17 22:35:23 +0200

    Update ImGuizmoImGuiCompat.h

--- Dependencies/imguizmo_compat/ImGuizmoImGuiCompat.h
+++ Dependencies/imguizmo_compat/ImGuizmoImGuiCompat.h
@@ -3,6 +3,10 @@
 
 #include "imgui.h"
 
+#if IMGUI_VERSION_NUM >= 19280
+#define AddPolyline(points, num_points, col, closed, thickness) AddPolyline(points, num_points, col, static_cast<float>(thickness), (closed) ? ImDrawFlags_Closed : 0)
+#endif
+
 namespace ImGui
 {
 
